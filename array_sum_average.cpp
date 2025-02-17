2de3b9e63a55b4c78b30b47b1bbb559c8f2d2d93
Hiruni Hemantha <123827294+hirunihem@users.noreply.github.com>
2025-02-17 15:17:15 +0530

    Update array_sum_average.cpp

--- array_sum_average.cpp
+++ array_sum_average.cpp
@@ -6,21 +6,21 @@ int main()
     int i,s,A;
     s=0;
 
-    int a[5];
-
-    cout << " Enter 5 Marks" << endl;
+    int a[5];  //creating array
+    
+    cout << " Enter 5 Marks" << endl; //input 5 marks
 
     for(i=0;i<5;i++)
     {
         cin >> a[i];
     }
 
-    for(i=0;i<5;i++)
+    for(i=0;i<5;i++)  //calculating sum of marks
     {
         s=s+a[i];
     }
     cout << s << endl;
-     A=s/5;
+     A=s/5;     //calculate average
     cout << A;
 
 
