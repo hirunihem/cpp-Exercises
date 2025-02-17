bb2147eef8ebd01fa3ae2531f55f69b709d1e0e8
Hiruni Hemantha <123827294+hirunihem@users.noreply.github.com>
2025-02-17 11:31:22 +0530

    simple-output

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//HIRUNI
+//08APRIL2022
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------" << endl;
+    cout << "Unit price  :  250.00            " << endl;
+    cout << "Quantity    :  5                 " << endl;
+    cout << "---------------------------------" << endl;
+    cout << "Total       :  1250.00           " << endl;
+    cout << "=================================" << endl;
+    return 0;
+}
